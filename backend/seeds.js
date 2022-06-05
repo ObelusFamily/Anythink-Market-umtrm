c4cf8aca71e1516a52483df103b149e25b60d4d0
renoneto <renoneto@gmail.com>
2022-06-04 18:53:04 -0700

    add seeds.js

---
+++ backend/seeds.js
@@ -0,0 +1,46 @@
+var mongoose = require("mongoose");
+require("./models/User");
+require("./models/Item");
+require("./models/Comment");
+require("./config/passport");
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.set("debug", true);
+
+var Item = mongoose.model("Item");
+var User = mongoose.model("User");
+var Comment = mongoose.model("Comment");
+
+// create 100 users, items and comments
+var addItems = async () => {
+    for (var i = 0; i<100; i++) {
+        let rand = Math.ceil(Math.random() * 100000);
+        let user = new User({
+            username: rand,
+            email: `${rand}@${rand}.com`
+        });
+        let item = new Item({
+            title: `random ${rand}`,
+            description: `random ${rand}`,
+            image: `https://picsum.photos/id/${rand}/200/300`,
+            tagList: '',
+        });
+        let comment = new Comment({
+            body: rand
+        })
+
+        // create user
+        let savedUser = await user.save();
+
+        // create item
+        item.seller = savedUser;
+        let savedItem = await item.save();
+
+        // create commment
+        comment.seller = savedUser;
+        comment.item = savedItem;
+        await comment.save();
+    }
+}
+
+addItems().then(process.exit);
